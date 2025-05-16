59cd0bfe24c128516e6663fa3a862d01645267a1
Tomiwa Philip <tomiwaphilip1100@gmail.com>
2025-05-16 03:26:58 +0100

    fix: refine network client configuration in coin.ts
    - Updated the type assertion for the client variable to explicitly include both 'base' and 'baseSepolia'.
    - Modified the transport method in the wallet client to use the default HTTP transport instead of the client RPC URL.

--- lib/content/coin.ts
+++ lib/content/coin.ts
@@ -5,7 +5,7 @@ import { networks } from "@/contract/config";
 import { base, baseSepolia } from "viem/chains";
 
 const network = networks.defaultNetwork === "base" ? base : baseSepolia;
-const client = networks[(networks.defaultNetwork as "base") || "baseSepolia"];
+const client = networks[networks.defaultNetwork as "base" | "baseSepolia"];
 const publicClient = createPublicClient({
   chain: network,
   transport: http(client.rpcUrl),
@@ -39,7 +39,7 @@ export async function createContentToken(
   const walletClient = createWalletClient({
     account,
     chain: network,
-    transport: http(client.rpcUrl),
+    transport: http(),
   });
 
   // Get ContentFactory contract address and ABI
